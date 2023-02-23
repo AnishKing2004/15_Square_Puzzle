dc3ecb72aba00505431d627c77615992f91b285e
AnishKing2004 <67561021+AnishKing2004@users.noreply.github.com>
2023-02-22 16:54:18 -0800

    Added some comments

--- app/src/main/java/com/example/a15_squarespuzzle/Game.java
+++ app/src/main/java/com/example/a15_squarespuzzle/Game.java
@@ -3,6 +3,7 @@ package com.example.a15_squarespuzzle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,25 +14,26 @@ import java.util.Random;
 
 public class Game extends AppCompatActivity {
 
-    private int emptyX = 3;
-    private int emptyY = 3;
-    private RelativeLayout group;
-    private Button[][] buttons;
-    private int[] tiles;
-    private Button shuffleButton;
+    private int emptyX = 4; //where the empty space is located
+    private int emptyY = 4; // where the empty space is located
+    private RelativeLayout group; //groups the buttons
+    private Button[][] buttons; //2D array of buttons
+    private int[] tiles; //array of tiles
+    private Button shuffleButton; //Shuffle button
 
 
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //runs these methods
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        loadViews();
-        loadNumbers();
-        generateNumbers();
+        views();
+        numbers();
+        createNum();
         loadDataTOViews();
+
 
 
     }
@@ -47,7 +49,9 @@ public class Game extends AppCompatActivity {
         buttons[emptyX][emptyY].setText("");
         buttons[emptyX][emptyY].setBackgroundColor(ContextCompat.getColor(this,R.color.emptyspace));
     }
-    private void generateNumbers(){
+    private void createNum(){
+        // generates random integers
+        // the while loop shuffles the array and swaps the values at random indexes
         int n = 15;
         Random random = new Random();
         while(n > 1) {
@@ -57,12 +61,12 @@ public class Game extends AppCompatActivity {
             tiles[n] = temp;
 
         }
-        if(!isSolvable())
-            generateNumbers();
+        if(!isSolvable()) //this method checks if the puzzle actually solvable
+            createNum();
 
 
     }
-    private boolean isSolvable() {
+    private boolean isSolvable() { //checks if the puzzel is solvable
         int countInversion = 0;
         for (int i = 0; i < 15; i++){
             for(int j = 0; j < i; j++){
@@ -73,14 +77,16 @@ public class Game extends AppCompatActivity {
         }
         return countInversion%2 == 0;
     }
-    private  void loadNumbers() {
+    private  void numbers() { //loads the numbers
         tiles = new int[16];
         for(int i = 0; i < group.getChildCount() - 1; i++){
             tiles[i] = i + 1;
         }
 
     }
-    private void loadViews(){
+    private void views(){
+        //places the buttons on the screen
+        //makes a two dimensional array to store references to each of the buttons in the grid
         group = findViewById(R.id.group);
         buttons = new Button[4][4];
         shuffleButton = findViewById(R.id.button_shuffle);
@@ -90,17 +96,20 @@ public class Game extends AppCompatActivity {
             buttons[i/4][i%4] = (Button)  group.getChildAt(i);
 
         }
-        shuffleButton.setOnClickListener(new View.OnClickListener() {
+        shuffleButton.setOnClickListener(new View.OnClickListener() { //generates a new random pattern when shuffle button is pressed
             @Override
             public void onClick(View view){
-                generateNumbers();
+                createNum();
                 loadDataTOViews();
 
             }
         });
     }
 
+
     public void buttonClick(View view){
+        //checks if the adjacent button is empty
+        // if it is empty the text and background color swap
         Button button = (Button) view;
         int x = button.getTag().toString().charAt(0)- '0';
         int y = button.getTag().toString().charAt(1)- '0';
@@ -112,30 +121,27 @@ public class Game extends AppCompatActivity {
             button.setBackgroundColor(ContextCompat.getColor(this,R.color.emptyspace));
             emptyX = x;
             emptyY = y;
-            checkWin();
+            winner(); // this method is called to check with player won
 
 
         }
     }
 
-    private void checkWin(){
-        boolean isWin = false;
+    private void winner(){ //checks if player won
+        boolean win = false;
         if (emptyX == 3 && emptyY == 3){
             for(int i = 0; i < group.getChildCount() - 1; i++) {
                 if (buttons[i / 4][i % 4].getText().toString().equals(String.valueOf(i + 1))){
-                    isWin = true;
+                    win = true;
                 }else{
-                    isWin = false;
+                    win = false;
                     break;
                 }
             }
         }
-        if (isWin) {
+        if (win) { //if user wins play the win message and send user back to home screen
             Toast.makeText(this,"Win", Toast.LENGTH_SHORT).show();
-            for (int i = 0; i < group.getChildCount(); i++) {
-                buttons[i/4][i%4].setClickable(false);
-
-            }
+            startActivity(new Intent(Game.this,Game.class));
             shuffleButton.setClickable(false);
 
 
